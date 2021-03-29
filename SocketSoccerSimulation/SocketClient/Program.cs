58a6febed78cdaad0e6dfab806b3d2a67ef1c876
SzilvasiPeter <peti.szilvasi95@gmail.com>
2021-03-29 18:51:56 +0200

    Simple Socket server client communication

---
+++ SocketSoccerSimulation/SocketClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 5500);
+            using (Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.Connect(endPoint);
+                using (NetworkStream stream = new NetworkStream(socket, true))
+                {
+                    int length = 10;
+                    byte[] data = new byte[length];
+                    Random random = new Random();
+                    for (int i = 0; i < length; i++)
+                    {
+                        data[i] = (byte)random.Next(255);
+                    }
+
+                    foreach (var singleByte in data)
+                    {
+                        Console.WriteLine(singleByte);
+                    }
+
+                    stream.WriteByte((byte)length);
+                    stream.Write(data, 0, length);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SocketSoccerSimulation/SocketServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 5500);
+            using (Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.Bind(endPoint);
+                socket.Listen(100);
+
+                while (Console.ReadLine() != "e")
+                {
+                    Socket acceptedSocket = socket.Accept();
+                    using (NetworkStream stream = new NetworkStream(acceptedSocket, true))
+                    {
+                        int length = stream.ReadByte();
+
+                        byte[] buffer = new byte[length];
+                        int remaining = length;
+                        int offset = 0;
+
+                        while (remaining > 0)
+                        {
+                            int numberOfBytesRead = stream.Read(buffer, offset, remaining);
+                            offset += numberOfBytesRead;
+                            remaining -= numberOfBytesRead;
+                        }
+
+                        foreach (var singleByte in buffer)
+                        {
+                            Console.WriteLine(singleByte);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
