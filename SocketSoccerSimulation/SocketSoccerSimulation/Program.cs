ba3eb91efe8abdea57b279f6507898c37093363a
SzilvasiPeter <peti.szilvasi95@gmail.com>
2021-03-29 18:25:30 +0200

    Init .net core application

---
+++ SocketSoccerSimulation/SocketSoccerSimulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocketSoccerSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
